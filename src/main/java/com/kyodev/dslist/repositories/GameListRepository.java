8b1e682547f7698eb88fdba2982501259cde21f3
bywilliams <willian.seu@gmail.com>
2024-12-12 16:43:31 -0300

    GET lists endpoint

---
+++ src/main/java/com/kyodev/dslist/repositories/GameListRepository.java
@@ -0,0 +1,7 @@
+package com.kyodev.dslist.repositories;
+
+import com.kyodev.dslist.entities.GameList;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameListRepository extends JpaRepository<GameList, Long> {
+}
